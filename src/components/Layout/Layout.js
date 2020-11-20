a9f54515a9a296f88999cd5932237ece690bea85
Paulo <8630187+papawlo@users.noreply.github.com>
2020-11-20 16:46:40 +0000

    129. Outputting Burger Ingredients Dynamically

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+
+const layout = props => {
+    return (
+        <Aux>
+            <div>
+                <p>
+                    Toolbar, Sidebar, Backdrop
+                </p>
+            </div>
+            <main className={classes.Content}>
+                {props.children}
+            </main>
+        </Aux>
+    );
+};
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Aux>
+                    <Burger ingredients={this.state.ingredients}>
+
+                    </Burger>
+                    <div>
+                        Builder Controls
+                    </div>
+                </Aux>
+            </div>
+        );
+    }
+}
+
+export default BurgerBuilder;
