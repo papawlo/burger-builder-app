98484287b9535cc7dce66ce7577b6418a5d2d6da
Paulo <8630187+papawlo@users.noreply.github.com>
2020-11-23 18:24:26 +0000

    147. Working on Responsive Adjustments

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import burgerLogo from '../../assets/images/burger-logo.png'
-import classes from './logo.css'
-const logo = () => {
+import classes from './Logo.css'
+const logo = (props) => {
     return (
-        <div className={classes.Logo}>
+        <div
+            className={classes.Logo}>
             <img src={burgerLogo} alt="MyBurger" />
-        </div>
+        </div >
     )
 }
 
