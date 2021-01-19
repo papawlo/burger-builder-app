b7b717813f9e375bb879e04d6856e160af039343
Paulo <8630187+papawlo@users.noreply.github.com>
2021-01-19 19:23:47 +0000

    312. Combining Reducers

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,8 +1,7 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    order: [],
-    purchasing: false,
+    orders: [],
     loading: false,
 }
 const reducer = (state = initialState, action) => {
