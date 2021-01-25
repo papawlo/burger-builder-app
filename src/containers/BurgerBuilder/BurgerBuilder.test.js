a45b9e5a4bbf9b29c8dccb76f24138dda9481b5b
Paulo <8630187+papawlo@users.noreply.github.com>
2021-01-25 19:18:30 +0000

    361. How to Test Redux

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -14,7 +14,7 @@ describe('<BurgerBuilder />', () => {
         }} />);
     });
 
-    it('should render >BuildControls when receiving ingredients', () => {
+    it('should render <BuildControls/> when receiving ingredients', () => {
         wrapper.setProps({ ings: { salad: 0 } });
         expect(wrapper.find(BuildControls)).toHaveLength(1);
     })
